c77aa17df2492fcf44a5e00ab8305713ee931e2d
Kedar Aitawdekar <kedar2a@gmail.com>
2018-10-22 01:10:35 +0530

    updated ALLOWED_HOST entry

--- data_gallery_project/settings.py
+++ data_gallery_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xlie&&t#-2qnjm$$-*+mal*pn0aa%j=79x@vhbhfpim6_uxpc)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
